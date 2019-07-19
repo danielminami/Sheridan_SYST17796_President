2b47c8c3159f4c294a43e0910f01a8bfd30d4dfe
danielminami <danielminami@hotmail.com>
2019-07-19 16:08:58 -0400

    Tasks divided and assigned!

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -18,7 +18,21 @@ import java.util.Scanner;
  */
 public class Game {
     
-       
+    /*
+    TODO:   Create all the Java Docs for all classes and Enums of the project
+    Comments: the assigned person can list all the tasks and subdivide
+    
+    AssignedTo: Muaz (Status: hold, wait until project ends)
+    */
+    
+    /*
+    TODO:   Create all the Unit Tests for all classes of the project
+            with boundary, good and bad tests.
+    Comments: the assigned person can list all the tasks and subdivide
+    
+    AssignedTo: Muaz (Status: hold, wait until project ends)
+    */
+    
     public static void main(String[] args){
     
         int playerNumber = 1;
@@ -31,6 +45,14 @@ public class Game {
         int playerTurn = 99;
         
         /*Getting the Player names and adding to the gamePlayers obj*/
+        /* 
+        TODO:   Implement the validation for player names, we don't
+                have a other criteria than not allowing same names.
+                Implement the player comparison in the Player Class equals
+                Method by overring it from parent Object
+        
+        AssignedTo: Stu (status: doing)
+        */
         do {
             try {
                 System.out.println("Enter the name of Player " + 
@@ -52,14 +74,29 @@ public class Game {
             p.setHand(hand);
         }
             
+        /*
+        TODO: Implement the Welcome Message when the 4 players are set.
+                It probably should be implemented in the main method
+        
+        AssignedTo: Muaz (not initiated)
+        */
         for (Player p: gamePlayers) {
             System.out.println(p);
         }
         
+       
         do {
             Trick trick = new Trick();
             if (playerTurn == 99)
                 playerTurn = 0;
+            
+            /*
+            TODO:   Setup the Trick properly (e.g copying the gamePlayers array
+                    into a new Object every new Trick).
+                    The class Trick might have to be addapted.
+            
+            AssignedTo: Stu
+            */
             
             do {
                 Turn turn = new Turn(gamePlayers.get(playerTurn));
@@ -68,27 +105,62 @@ public class Game {
                 /*Getting the Player movement*/
                 do {
                     try {
+                        /*
+                        TODO:   Improve the user message explaining how to play
+                                correctly.
+                        
+                        AssignedTo: Muaz (not initiated)
+                        */
+                        
                         System.out.println("Enter 'p' to pass or type the "
                                 + "card(s) index(es), separating by comma: ");
                         
-                        //stub - Prints the hand with indexes
+                        /*
+                        TODO:   Implement the toString method in hand that
+                                displays the current player hand
+                        
+                        AssignedTo: Muaz (not initiated)
+                        */        
                         
                         in = new Scanner(System.in);
                         playerEntry = in.nextLine();
                         if (playerEntry.equals("p")){
-                            //when user pass
+                            /*
+                            TODO:   Implement the method pass() in the Turn 
+                                    Class. It should also update the Turn object
+                                    and check for the whether the Turn isOver.
+                            
+                            AssignedTo: Daniel (not initiated)
+                            */
                         } else {
-                            //when users play
+                            /*
+                            TODO:   Test the user Entry function for bad,
+                                    good and boundary tests
+                            
+                            AssignedTo: Muaz (not initiated)
+                            */
                             
                             //placing the users entry in a int Array
                             int nextIndex = 0;
                             for (int i = 0; i < playerEntry.length(); i++) {
                                 if (playerEntry.charAt(i) == ',') {
-                                    playerMove.add(Integer.parseInt(playerEntry.subSequence(nextIndex, i-1).toString()));
+                                    playerMove.add(Integer.parseInt(playerEntry.
+                                            subSequence(nextIndex, i-1).
+                                            toString()));
                                     nextIndex = i+1;
                                 }
                             }
                             
+                            /*
+                            TODO:   Implement the playTurn method in the Turn
+                                    Class. It users the Hand Class and Board
+                                    Class to make sure the movement is valid.
+                                    By the end of the execution, updates the
+                                    objects hand and board.
+                                    It should also update the Turn object
+                            
+                            AssignedTo: Daniel (doing)
+                            */                            
                             turn.playTurn(playerMove, board);
                             
                             
@@ -100,14 +172,30 @@ public class Game {
                         System.out.println("Invalid Movement.");
                     }
                 } while (playerNumber <= 4);
+                /* TODO:    check whether the Trick is over, otherwise, update 
+                            the Trick for next Turn;
                 
+                AssignedTo: Stu
+                */
                 
             } while (trick.isIsActive());
-        
+
+            /* 
+            TODO:   check whether the Round is over, otherwise, update 
+                    the Trick for next Turn;
+            
+            AssignedTo: Stu (not initiated)
+            */            
+            
         } while (round.isIsActive());
     
+        /* 
+        TODO:   Finish the game:
+                    Display the winner
+                    Close objects
         
-        
+        AssignedTo: Muaz (not initiated)
+        */
     }
     
 }
